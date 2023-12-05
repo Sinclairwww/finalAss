0f3467dbf0c2edd152fdf13cdf2944b6cb06fb71
Sinclairwww <90683011+Sinclairwww@users.noreply.github.com>
2023-12-05 23:03:25 +0800

    finish bj spider

--- finalAss/spiders/bj.py
+++ finalAss/spiders/bj.py
@@ -10,8 +10,8 @@ class BjSpider(scrapy.Spider):
     name = "bj"
     allowed_domains = ["bj.lianjia.com"]
     start_urls = ["https://bj.lianjia.com/zufang/#contentList"]
-    # prefix = "https://bj.lianjia.com/zufang/pg"
-    # index = 2
+    prefix = "https://bj.lianjia.com/zufang/pg"
+    index = 2
 
     custom_settings = {
         "ITEM_PIPELINES": {
@@ -21,17 +21,21 @@ class BjSpider(scrapy.Spider):
     }
 
     def parse_item(self, selector):
-        isOK = True
         item = BJItem()
-        info = selector.css("a.content__list--item--aside[title]::attr(title)").extract_first()
+        info = selector.css(
+            "a.content__list--item--aside[title]::attr(title)"
+        ).extract_first()
         info_list = info.split(" ")
         item["名称"] = info_list[0][3:]
 
         typeList = [s for s in info_list if "室" in s or "房间" in s]
         item["房型"] = typeList[0] if len(typeList) > 0 else ""
 
-        orientationList = [s for s in info_list if
-                           len(s) in range(1, 4) and ("东" in s or "南" in s or "西" in s or "北" in s)]
+        orientationList = [
+            s
+            for s in info_list
+            if len(s) in range(1, 4) and ("东" in s or "南" in s or "西" in s or "北" in s)
+        ]
         item["朝向"] = orientationList[0] if len(orientationList) > 0 else ""
 
         info = selector.css("p.content__list--item--des a::text").getall()
@@ -46,17 +50,19 @@ class BjSpider(scrapy.Spider):
         areaList = [s for s in info if "㎡" in s]
         item["面积"] = areaList[0].strip() if len(areaList) > 0 else ""
 
-        item["价格"] = selector.css('span.content__list--item-price em::text').get()
+        item["价格"] = selector.css("span.content__list--item-price em::text").get()
         item["单位面积价格"] = ""
-        yield item
+        return item
 
     def parse(self, response):
         for each in response.css(".content__list--item"):
             item = self.parse_item(each)
             yield item
 
-        next_page = response.css("#content > div.content__article > div.content__pg > a.next").extract_first()
-        if next_page:
+        if self.index <= 100:
+            next_page = self.prefix + str(self.index) + "/" + "#contentList"
+            self.index += 1
             yield scrapy.Request(next_page, callback=self.parse)
         else:
             return None
+
