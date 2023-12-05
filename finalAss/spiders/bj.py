4200f16d43d01bb9d130946f5dfee1becb228220
Xianke Wu <wu.xianke@mech-mind.net>
2023-12-05 14:42:44 +0800

    delete some unused parts

--- finalAss/spiders/bj.py
+++ finalAss/spiders/bj.py
@@ -43,19 +43,4 @@ class BjSpider(scrapy.Spider):
             areaList = [s for s in info if "㎡" in s]
             item["面积"] = areaList[0].strip() if len(areaList) > 0 else ""
             pass
-            # item = NewHouseItem()
-            # item["楼盘名称"] = each.css(".resblock-name .name::text").get().strip()
-            # item["类型"] = each.css(".resblock-type::text").get().strip()
-            # item["地理位置"] = each.css(".resblock-location a::text").get().strip()
-            # item["房型"] = each.css(".resblock-room span::text").getall()
-
-            # # 消除前三个多余字符
-            # area = each.css(".resblock-area span::text").get()
-            # item["面积"] = "" if area is None else area
-
-            # item["均价"] = each.css(".main-price span::text").get()
-            # total = each.css(".second::text").get()
-            # item["总价"] = "" if total is None else total
-            if isOK:
-                yield item
         pass
